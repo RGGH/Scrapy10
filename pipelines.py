e8e4aa9cf8a367972ecc31672c8a3763ac9a8731
Red and Green <62441426+RGGH@users.noreply.github.com>
2020-10-21 14:45:34 +0100

    Branch Version for MySQL
    Works fine, but need to change PRICE and STAR RATING to DECIMAL

--- pipelines.py
+++ pipelines.py
@@ -3,10 +3,14 @@
 #|r|e|d|a|n|d|g|r|e|e|n|.|c|o|.|u|k|
 #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
-import sqlite3
+
+import sys
+import mysql.connector
+from mysql.connector import errorcode
 from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
-
+from scrapy.crawler import CrawlerProcess
+     
 class AmzPipeline(object):
 
     def __init__(self):
@@ -14,33 +18,48 @@ class AmzPipeline(object):
         self.create_table()
     
     def create_conn(self):
-        self.conn = sqlite3.connect("test.db")
+
+        # Connect to DB
+        try:
+            self.conn = mysql.connector.connect(   
+                                    user = 'user1',
+                                    passwd = 'password1',
+                                    host = 'localhost',
+                                    port=3306,
+                                    database ='amz'
+            )
+        except mysql.Error as e:
+            print(f"Error connecting to DB Platform: {e}")
+            sys.exit(1)
+                                                             
         self.curr = self.conn.cursor()
-        
+     
     def create_table(self):
-        self.curr.execute("""DROP TABLE IF EXISTS books_sqlite""")
-        self.curr.execute("""create table books_sqlite (
-            title text,
-            author text,
-            star_rating text,
-            book_format text,
-            price text,
-            cover_image text
-            )""")
+        self.curr.execute("""DROP TABLE IF EXISTS amzbooks""")
+        self.curr.execute("""CREATE TABLE IF NOT EXISTS amzbooks (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            author VARCHAR(255),
+            star_rating VARCHAR(40),
+            book_format VARCHAR(255),
+            price VARCHAR(9),
+            cover_image VARCHAR(255))
+            """)
             
     def process_item(self,item,spider):
         
+        # Drop anything that is NOT a paperback
         adapter = ItemAdapter(item)
         if adapter.get('book_format'):
             if adapter['book_format'] == 'Paperback':
                 self.store_db(item)
             else:
                 raise DropItem(f"Not a paperback book {item}")
-         
+
     def store_db(self,item):
-        myquery = """INSERT into books_sqlite 
+        myquery = """INSERT into amzbooks 
         (title, author,star_rating,book_format,price, cover_image) 
-        values (?,?,?,?,?,?)
+        values (%s,%s,%s,%s,%s,%s)
         """
         val=(
             item.get('title'),
@@ -50,6 +69,8 @@ class AmzPipeline(object):
             item.get('price'),
             item.get('cover_image')
             )
-        
         self.curr.execute(myquery, val)
         self.conn.commit()
+
+        # to do : add conn.close
+     
