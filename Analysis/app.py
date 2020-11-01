f05a5d333b3db4cf63e523a57b35723322db8707
Red and Green <62441426+RGGH@users.noreply.github.com>
2020-11-01 23:57:10 +0000

    Update app.py

--- Analysis/app.py
+++ Analysis/app.py
@@ -1,31 +1,29 @@
-# app.py
+# app.py - make a graph from MySQL data from Scrapy
+
 import matplotlib.pyplot as plt
 import pymysql
 import pandas as pd
+plt.style.use('ggplot')
 
 # connect to db
-dbcon = pymysql.connect("localhost", "user1", "password1", "amz")
+
+dbcon = pymysql.connect("localhost","user1","password1","amz")
 
 try:
     SQL_Query = pd.read_sql_query(
-        '''select
-        author,
-        title,
-        price
-        from amzbooks''',dbcon)
+        '''SELECT star_rating, price FROM amzbooks2 WHERE star_rating > 1 ORDER BY price ASC''', dbcon 
+    )
 
-    df = pd.DataFrame(SQL_Query, columns=['star_rating', 'title', 'price'])
+    df = pd.DataFrame(SQL_Query, columns=['star_rating','price'])
     print(df)
-    print('The data type of df is: ', type(df))
-   
 
-except:
-    print("Error: unable to convert the data - check your code")
+    df.plot.bar( x = 'price', y = 'star_rating', color="green")
+    plt.ylabel('Rating')
+    plt.xlabel('Price')
+    plt.title("Ratings v Price - Amazon Books")
+    plt.show()
 
+except: 
+    print("Error - unable to connect / convert the data - check connection and code")
 
-    
 dbcon.close()
-
-# work in progress
-df.plot(x="star_rating", y="price")
-plt.show()
